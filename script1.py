afe761bfbc627ac32e3d9697af856b5258fa7a9f
PranavJ <pranavjnam@gmail.com>
2025-08-04 11:49:47 +0530

    Initial push V1

---
+++ script1.py
@@ -0,0 +1,12 @@
+
+def favourate_food(food):
+    print(f"Your favourate food is {food} ")
+
+
+def main():
+    print("This is script1")
+    favourate_food("Biriyani")
+    print("Byee")
+
+if __name__ == '__main__':
+    main()
---
+++ script2.py
@@ -0,0 +1,13 @@
+
+from script1 import *
+def favourate_drink(drink):
+    print(f"Your favourate drink is {drink}")
+
+def main():
+    print("This is script2")
+    favourate_food("puttu")
+    favourate_drink("mango shake")
+    print("Byee")
+
+if __name__ == '__main__':
+    main()
